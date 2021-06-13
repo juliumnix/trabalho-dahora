69c9d254a676769eaf505418f17cb2336043794b
juliumnix <juliumnix@gmail.com>
2021-06-13 18:03:57 -0300

    adicionado classe de Computador, Fonte, Headeset, Mouse, PlacaDeVideo, PlacaMae e Processador

---
+++ Trabalho/src/PlacaDeVideo.java
@@ -0,0 +1,70 @@
+public class PlacaDeVideo extends Produto {
+    private String interfaceMemoria;
+    private boolean alimentacao;
+    private int memoria;
+
+    public PlacaDeVideo(String modelo, float valor, String descricao, String marca, String categoria,
+                        String interfaceMemoria, boolean alimentacao, int memoria) {
+        super(modelo, valor, descricao, marca, categoria);
+        this.interfaceMemoria = interfaceMemoria;
+        this.alimentacao = alimentacao;
+        this.memoria = memoria;
+    }
+
+    public String getInterfaceMemoria() {
+        return interfaceMemoria;
+    }
+
+    public boolean setInterfaceMemoria(String interfaceMemoria) {
+        if (interfaceMemoria.isBlank()) {
+            return false;
+        }
+        this.interfaceMemoria = interfaceMemoria;
+        return true;
+    }
+
+    public boolean getAlimentacao() {
+        return alimentacao;
+    }
+
+    public void setAlimentacao(boolean alimentacao) {
+        this.alimentacao = alimentacao;
+    }
+
+    public int getMemoria() {
+        return memoria;
+    }
+
+    public boolean setMemoria(int memoria) {
+        if (memoria >=0) {
+            this.memoria = memoria;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getModelo() {
+        return super.getModelo();
+    }
+
+    @Override
+    public float getValor() {
+        return super.getValor();
+    }
+
+    @Override
+    public String getDescricao() {
+        return super.getDescricao();
+    }
+
+    @Override
+    public String getMarca() {
+        return super.getMarca();
+    }
+
+    @Override
+    public String getCategoria() {
+        return super.getCategoria();
+    }
+}
